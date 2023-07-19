073f84f5dd24ed75bceefd15b320bba9621eaff9
Benjamin Nass <bnass@hyl.cl>
2023-07-19 15:07:53 -0400

    feat: element of product created

---
+++ src/components/Guitarra.vue
@@ -0,0 +1,30 @@
+<script setup>
+// registro del objeto "Guitarra", llamado en App.vue
+const props = defineProps({
+    guitarra: {
+        type: Object,
+        required: true
+    }
+});
+
+// registro del evento incrementar, el cual se encuentra en App.vue
+defineEmits(['agregar-carrito'])
+
+
+</script>
+
+<template>
+    <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+        <div class="col-4">
+            <img class="img-fluid" :src="'/img/' + guitarra.imagen + '.jpg'" :alt="'imagen guitarra' + guitarra.nombre">
+        </div>
+        <div class="col-8">
+            <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
+            <p>{{ guitarra.descripcion }}</p>
+            <p class="fw-black text-primary fs-3" > ${{ guitarra.precio }}</p>
+            <button @click="$emit('agregar-carrito', guitarra)" type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
+        </div>
+    </div><!-- FIN GUITARRA -->
+</template>
+
+<style lang="scss" scoped></style>
