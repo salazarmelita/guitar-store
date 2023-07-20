d4b0e20aa2e3b465c38a9fa19c4c5438e1ba34dc
salazarmelita <asalazar@hyl.cl>
2023-07-20 10:18:08 -0400

    feat: keys added

--- src/App.vue
+++ src/App.vue
@@ -73,7 +73,7 @@ const vaciarCarrito = (guitarra) => {
     <main class="container-xl mt-5">
         <h2 class="text-center">Nuestra Colección</h2>
         <div class="row mt-5">
-            <Guitarra v-for="guitarra in guitarras" v-bind:guitarra="guitarra" @agregar-carrito="agregarCarrito" />
+            <Guitarra v-for="guitarra in guitarras" :key="guitarra.id" v-bind:guitarra="guitarra" @agregar-carrito="agregarCarrito" />
         </div>
     </main>
     <Footer />
